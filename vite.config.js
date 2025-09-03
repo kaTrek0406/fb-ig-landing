212512cde1f94d0976ad46fc78301648d24d7075
Vladislav Caireac <124244962+kaTrek0406@users.noreply.github.com>
2025-09-03 14:07:02 +0300

    init: vite react landing

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Вариант A: укажи ИМЯ РЕПОЗИТОРИЯ (если Pages на https://username.github.io/repo)
+  base: '/fb-ig-landing/',
+  // Вариант B (универсальный): относительные пути, если хостишь где угодно
+  // base: './',
 })
